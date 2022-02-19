2f2c0f915599ea40d79c735de314167dcaaab26d
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-02-19 19:08:13 +0530

    gitignore file

--- oopjava/UCSDUnfoldingMaps/src/practice/LocationTester.java
+++ oopjava/UCSDUnfoldingMaps/src/practice/LocationTester.java
@@ -1,5 +1,7 @@
 package practice;
 
 public class LocationTester {
-
+	public static void main(String[] args) {
+		double d = -77.0;
+	}
 }
--- oopjava/UCSDUnfoldingMaps/src/practice/SimpleLocation.java
+++ oopjava/UCSDUnfoldingMaps/src/practice/SimpleLocation.java
@@ -1,5 +1,11 @@
 package practice;
 
+// Basic SimpleLocation class
 public class SimpleLocation {
-
+	public double latitude;
+	public double longitude;
+	public SimpleLocation(double latitude, double longitude) {
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
 }
