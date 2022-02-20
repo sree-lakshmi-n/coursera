b50b58a91f37af69630009a4e330373e4046d208
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-02-20 11:51:30 +0530

    merged ObjectAssignTester.java to LocationTester.java

--- oopjava/UCSDUnfoldingMaps/src/practice/ObjectAssignTester.java
+++
@@ -1,9 +0,0 @@
-package practice;
-
-//Familiarising with memory models and variable assignment for objects
-public class ObjectAssignTester {
-	public static void main(String[] args) {
-		int var1 = 52;
-		
-	}
-}
