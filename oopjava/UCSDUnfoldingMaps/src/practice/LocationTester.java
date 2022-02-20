23e599c215b1fd1b83cae354f5f388d8ecc3877a
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-02-20 11:51:54 +0530

    Familiarising with memory models of objects

--- oopjava/UCSDUnfoldingMaps/src/practice/LocationTester.java
+++ oopjava/UCSDUnfoldingMaps/src/practice/LocationTester.java
@@ -1,7 +1,17 @@
 package practice;
 
+// Familiarising with memory models of objects
 public class LocationTester {
 	public static void main(String[] args) {
 		double d = -77.0;
+		SimpleLocation lima = new SimpleLocation(-12.0,d);
+		SimpleLocation washDC = new SimpleLocation(38.9,lima.longitude);
+		lima.longitude = -12.04;
+		System.out.println("lima \n latitude: "+lima.latitude+"\n longitude: "+lima.longitude);
+		System.out.println("washDC \n latitude: "+washDC.latitude+"\n longitude: "+washDC.longitude);
+		lima = washDC;
+		lima.latitude = -8.3;
+		System.out.println("lima \n latitude: "+lima.latitude+"\n longitude: "+lima.longitude);
+		System.out.println("washDC \n latitude: "+washDC.latitude+"\n longitude: "+washDC.longitude);
 	}
 }
